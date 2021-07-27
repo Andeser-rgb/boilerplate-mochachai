f9725a8de73a0618c0cdc77fe080780514c4b1f7
Andeser-rgb <andeser@protonmail.com>
2021-07-27 14:12:37 +0200

    2- Test number 5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -64,9 +64,11 @@ suite("Functional Tests", function () {
 });
 
 const Browser = require("zombie");
+Browser.site = 'https://boilerplate-mochachai.immeprobably.repl.co';
 
 suite("Functional Tests with Zombie.js", function () {
-
+  const browser = new Browser();
+  suiteSetup(done => browser.visit('/', done));
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
